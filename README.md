18664bc08af8632a2107db76ab69378c5576f8d5
Estrada <kevin.a.estrada@capgemini.com>
2023-12-04 17:08:54 -0600

    Finished all of day 2

--- README.md
+++ README.md
@@ -4,8 +4,8 @@
 - Part 2: Need to come back to it and figure out some way to solve.
 
 ## Day 2:
-- Part 1: TODO
-- Part 2: TODO
+- Part 1: Finished and got gold star.
+- Part 2: Finished and got gold star.
   
 ## Day 3:
 - Part 1: TODO
---
+++ src/Day2/Day2PartTwo.java
@@ -0,0 +1,46 @@
+package Day2;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+import utils.ReadFile;
+
+public class Day2PartTwo {
+    public static void main(String[] args) {
+        ReadFile readFile = new ReadFile(
+                "C:\\Users\\kestrada\\Documents\\Advent Of Code\\advent-of-code\\src\\Day2\\File2.txt");
+        int sum = 0;
+
+        for (String line : readFile.getScannerIterable()) {
+            // System.out.println(Arrays.toString(line.split(":")));
+            String[] splitLine = line.split(":");
+            String gameName = splitLine[0];
+            // System.out.println("Starting " + gameName);
+            Map<String, Integer> colorMap = new HashMap<>();
+            colorMap.put("red", 0);
+            colorMap.put("green", 0);
+            colorMap.put("blue", 0);
+            for (String games : splitLine[1].split(";")) {
+                // System.out.println("Doing game: " + games);
+                for (String singleGame : games.split(",")) {
+                    singleGame = singleGame.strip();
+                    String gameColor = singleGame.split(" ")[1];
+                    // System.out.println("Game color: " + gameColor);
+                    // System.out.println("Game Number: " + singleGame.split(" ")[0]);
+                    int gameColorNumber = Integer.parseInt(singleGame.split(" ")[0]);
+                    if (colorMap.get(gameColor) < gameColorNumber) {
+                        colorMap.put(gameColor, gameColorNumber);
+                    }
+                }
+            }
+            int multiplcationNum = 1;
+            for(Map.Entry<String,Integer> entry : colorMap.entrySet()) {
+                // System.out.println("Least amount of colors that is needed for " + entry.getKey() + " is: " + entry.getValue());
+                multiplcationNum *= entry.getValue();
+            }
+            sum += multiplcationNum;
+        }
+        System.out.println("Sum is: " + sum);
+    }
+}
